ee4f3d67f37c0f8e839c30e561df600fcb2f07cf
Ilya Chernov <i.chernov@hotmail.com>
2023-11-24 01:06:52 +0200

    Верстка

--- resources/views/assets/header.blade.php
+++ resources/views/assets/header.blade.php
@@ -3,12 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,600;1,400&display=swap" rel="stylesheet">
 
 	<!-- FAVICON -->
-	<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png">
 	<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
 	<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
 	<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
@@ -26,26 +23,9 @@
 	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 	<meta name="theme-color" content="#ffffff">
 
-		<!-- Yandex.Metrika counter -->
-	<script type="text/javascript" >
-	   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-	   m[i].l=1*new Date();
-	   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-	   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-	   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-	   ym(94136155, "init", {
-			clickmap:true,
-			trackLinks:true,
-			accurateTrackBounce:true,
-			webvisor:true
-	   });
-	</script>
-	<noscript><div><img src="https://mc.yandex.ru/watch/94136155" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-	<!-- /Yandex.Metrika counter -->
     @yield('head')
     <!-- CSS Stylesheet -->
-    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="css/stylesheet.css">
 
 
     <title>@yield('title')</title>
@@ -58,16 +38,18 @@
                 <div class="menu">
                     <div class="menu__logo">
                         <a href="{{ route('home')}}">
-                        <img src="./media/logo.png" alt="Логотип">
+                        <img src="./img/menu/logo-white.png" alt="Логотип">
                         </a>
                     </div>
 
                     <div class="menu__nav">
                         <ul id="menuNav">
-                            <li><a href="{{ route('home')}}">Главная</a></li>
-                            <li><a href="{{ route('about')}}">О медцентре</a></li>
-                            <li><a href="{{ route('our-services')}}">Услуги</a></li>
-                            <li><a href="{{ route('home')}}#contacts">Контакты</a></li>
+                            <li><a href="{{ route('home')}}" class="menu_links">Главная страница</a></li>
+                            <li><a href="{{ route('our-services')}}" class="menu_links">Услуги</a></li>
+                            <li><a href="{{ route('home')}}" class="menu_links">Цены</a></li>
+                            <li><a href="{{ route('about')}}" class="menu_links">О медицинском центре</a></li>
+                            <li><a href="{{ route('home')}}#contacts" class="menu_links">Контакты</a></li>
+                            <li><a href="{{ route('home')}}" class="menu_links">Оставить заявку</a></li>
                         </ul>
                     </div>
 
